006fa6df8b815b927baa9f45e2feb001b03914f6
senliontec <senliontec@163.com>
2022-08-02 18:13:33 +0800

    fix

--- get_repository.py
+++ get_repository.py
@@ -1,6 +1,14 @@
 
 import os
 
+def mkdir(path):
+    folder = os.path.exists(path)
+    if not folder:
+        os.makedirs(path)
+        print("Success created new folder")
+    else:
+        print("folder already exist")
+
 
 def get_repositoryname(filename):
     with open(filename,'r') as f:
@@ -8,12 +16,14 @@ def get_repositoryname(filename):
 
         for i in range(0,len(contents) // 4):
             key  = contents[i * 4 + 1].strip()
+            directory ='./src/' + key.split('/')[0]
+            mkdir(directory)
             type = contents[i * 4 + 2].strip()
             url  = contents[i * 4 + 3].replace('url:','').strip()
             version = contents[i * 4 + 4].replace('version:','').strip()
             command = 'git clone -b {} --recursive {}'.format(version, url) + ' --config "http.proxy=127.0.0.1:7890"'
             retval = os.getcwd()
-            os.chdir('./src/')
+            os.chdir(directory)
             print('{} :--------------------------'.format(i))
             print(command)
             os.system(command)
