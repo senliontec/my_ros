8caf8719c70307251d2942d6f7cdf31c11122162
senliontec <senliontec@163.com>
2022-08-02 10:15:46 +0800

    first commit

---
+++ installros.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# 1. Set locale
+
+apt update && apt install locales -y
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# 2. Add the ROS 2 apt repository
+
+# apt install software-properties-common -y
+# add-apt-repository universe
+
+
+
+# 3. add the ROS 2 apt repository to your system. First authorize our GPG key with apt.
+
+
+apt update &&  apt install curl gnupg lsb-release -y
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+
+# 4. Then add the repository to your sources list.
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+
+# 5. Install development tools and ROS tools
+
+apt update && apt install -y \
+  build-essential \
+  cmake \
+  git \
+  python3-colcon-common-extensions \
+  python3-flake8 \
+  python3-flake8-blind-except \
+  python3-flake8-builtins \
+  python3-flake8-class-newline \
+  python3-flake8-comprehensions \
+  python3-flake8-deprecated \
+  python3-flake8-docstrings \
+  python3-flake8-import-order \
+  python3-flake8-quotes \
+  python3-pip \
+  python3-pytest \
+  python3-pytest-cov \
+  python3-pytest-repeat \
+  python3-pytest-rerunfailures \
+  python3-rosdep \
+  python3-setuptools \
+  python3-vcstool \
+  wget
+
+# 6. Get ROS 2 code
+
+mkdir -p ~/ros2_humble/src
+cd ~/ros2_humble
+wget https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos
+vcs import src < ros2.repos
+
+# 7. Install dependencies using rosdep
+
+apt upgrade
+
+rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+
+# 8. Build the code in the workspace
+
+cd ~/ros2_humble/
+colcon build --symlink-install
+
+# 9. Environment setup
+
+# Replace ".bash" with your shell if you're not using bash
+# Possible values are: setup.bash, setup.sh, setup.zsh
+. ~/ros2_humble/install/local_setup.bash
